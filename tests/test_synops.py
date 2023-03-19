2ca9f7b875f851b309ee4ce6df0954fbe8e84413
Gregor Lenz <gregor.lenz@synsense.ai>
2023-03-19 21:03:03 +0100

    add another test for conv layers with approximate float fanout

--- tests/test_synops.py
+++ tests/test_synops.py
@@ -23,6 +23,32 @@ def test_synops_conv_output():
     assert epoch_synops["synops"] == 4.5
 
 
+def test_synops_conv_output():
+    fanout = torch.tensor(3)
+    output = torch.ones((2, 1, 3, 3))
+    output2 = torch.ones((2, 1, 3, 3)) * 2
+
+    metric = snnmetrics.SynOps(fanout=fanout)
+
+    batch1_synops = metric(output)
+    assert (batch1_synops["synops_per_neuron"] == output.mean(0) * fanout).all()
+    assert batch1_synops["synops"] == (output.mean(0) * fanout).sum()
+
+    batch2_synops = metric(output2)
+    assert (batch2_synops["synops_per_neuron"] == output2.mean(0) * fanout).all()
+    assert batch2_synops["synops"] == (output2.mean(0) * fanout).sum()
+
+    epoch_synops = metric.compute()
+    assert (
+        epoch_synops["synops_per_neuron"]
+        == (output.mean(0) + output2.mean(0)) * fanout / 2
+    ).all()
+    assert (
+        epoch_synops["synops"]
+        == ((output.mean(0) + output2.mean(0)) * fanout / 2).sum()
+    )
+
+
 def test_synops_linear_output():
     fanout = torch.tensor(100)
     batch_size = 2
